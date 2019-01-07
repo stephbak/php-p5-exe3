bfca77d695ae093fb750d1838e8a22474c0e75dd
stephbak <stephane.bakum@sfr.fr>
2019-01-07 10:33:49 +0100

    p5 exe3

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$calendar = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre')
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="style.css" />
+  <title>php partie5</title>
+</head>
+<body>
+  <table>
+    <td><?= $calendar[5] ?></td>
+  </table>
+</body>
+</html>
